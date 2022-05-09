05a78da8aeffe0ae3754a6964c32b75364a674e6
Ronan <r.dhersignerie@gmail.com>
2022-05-09 15:03:12 +0200

    initial commit

---
+++ src/System.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rdher on 09/05/2022.
+//
+
+#include "System.h"
---
+++ src/System.h
@@ -0,0 +1,14 @@
+//
+// Created by rdher on 09/05/2022.
+//
+
+#ifndef CMD_ENGINE_SYSTEM_H
+#define CMD_ENGINE_SYSTEM_H
+
+
+class System {
+
+};
+
+
+#endif //CMD_ENGINE_SYSTEM_H
