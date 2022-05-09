46f6ab09c28f878b82706dfd5f1fa084a58e4a16
Ronan <r.dhersignerie@gmail.com>
2022-05-09 23:52:45 +0200

    Add FixedDeltaTime

--- src/Engine/Time.h
+++ src/Engine/Time.h
@@ -9,20 +9,41 @@
 #include <functional>
 
 namespace Time {
-   static std::chrono::time_point<std::chrono::high_resolution_clock> startTime, endTime;
-   static std::chrono::duration<float> elapsedTime;
+   static std::chrono::time_point<std::chrono::high_resolution_clock> startTime, endTime,fixedStartTime, fixedEndTime;
+   static std::chrono::duration<float> elapsedTime, fixedElapsedTime;
 
     static void Start(){
-        startTime = endTime = std::chrono::high_resolution_clock ::now();
+        startTime = endTime = fixedStartTime = fixedEndTime = std::chrono::high_resolution_clock::now();
     }
 
+    /**
+     * Calculate DeltaTime
+     */
     static void Update(){
         startTime = std::chrono::high_resolution_clock ::now();
         elapsedTime = startTime - endTime;
         endTime = startTime;
     }
 
+    /**
+     * The interval in seconds from the last frame to the current one (Read Only).
+     * @return
+     */
     static float DeltaTime(){
+        return elapsedTime.count();
+    }
+
+    static void FixedUpdate(){
+        fixedStartTime = std::chrono::high_resolution_clock::now();
+        fixedElapsedTime = fixedStartTime - fixedEndTime;
+        fixedEndTime = fixedStartTime;
+    }
+
+    /**
+     * TThe interval in seconds at which "physics" and other fixed frame rate updates (ReadOnly)
+     * @return
+     */
+    static float FixedDeltaTime(){
         return elapsedTime.count();
     }
 }
