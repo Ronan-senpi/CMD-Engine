8b81e66b20ea8a252d58b5591417f155ebd92bcc
Antoine Pavy <antoine.pavy92@gmail.com>
2022-05-09 18:13:49 +0200

    Map reading complete

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "src/MapLoader.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+	MapLoader loader;
+	std::string mapFile = "map1.txt";
+	loader.LoadMap(mapFile);
     return 0;
 }
